74bacb167727ffb618bf38b5edc01bae2e78ad70
yifanmoka <yifanmoka@qq.com>
2026-06-11 21:00:02 +0800

    这是我的博客

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,21 +6,21 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "一帆摩卡",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "yifanmoka",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://yifanmoka.com",
 
 	// 站点描述
 	description:
-		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"这是yifanmoka的个人博客，一个清新美观的个人博客。",
 
 	// 站点关键词
 	keywords: [
-		"Firefly",
+		"yifanmoka",
 		"Fuwari",
 		"Astro",
 		"ACGN",
